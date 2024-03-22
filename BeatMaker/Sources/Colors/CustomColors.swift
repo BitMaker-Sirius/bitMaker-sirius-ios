c300c532adde5b1b30cc5883a37279eb9db74628
irinapechik <irinapechik@gmail.com>
2024-03-22 08:35:26 +0300

    added custom colors

---
+++ BeatMaker/Sources/Colors/CustomColors.swift
@@ -0,0 +1,15 @@
+//
+//  CustomColors.swift
+//  BeatMaker
+//
+//  Created by Печик Ирина on 22.03.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let progressViewColor = Color(uiColor: UIColor(red: 122/255, green: 138/255, blue: 169/255, alpha: 1))
+    static let audioTrackColor = Color(uiColor: UIColor(red: 122/255, green: 138/255, blue: 169/255, alpha: 1))
+    static let chosenSoundColor = Color(uiColor: UIColor(red: 4/255, green: 33/255, blue: 89/255, alpha: 1))
+    static let soundCellsColor = Color(uiColor: UIColor(red: 122/255, green: 138/255, blue: 169/255, alpha: 0.1))
+}
