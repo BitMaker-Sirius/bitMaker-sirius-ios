40fb349d58c8a4144598a9614651a85277e979ae
irinapechik <irinapechik@gmail.com>
2024-03-27 13:43:43 +0300

    added files

---
+++ BeatMaker/Presentation/ProjectEditor/Subviews/TrackVisualization/TrackVisualizationView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackVisualizationView.swift
+//  BeatMaker
+//
+//  Created by Ирина Печик on 27.03.2024.
+//
+
+import SwiftUI
+
+struct TrackVisualizationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackVisualizationView()
+}
---
+++ BeatMaker/Presentation/ProjectEditor/Subviews/TrackVisualization/TrackVisualizationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TrackVisualizationViewModel.swift
+//  BeatMaker
+//
+//  Created by Ирина Печик on 27.03.2024.
+//
+
+import Foundation
